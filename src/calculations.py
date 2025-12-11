2230fc19cba5f5db53dffdb984cdc148f0c75d6a
webgeng <gtd.bs.web@gmail.com>
2025-12-11 07:26:50 +0000

    Add Python coverage workflow

---
+++ src/calculations.py
@@ -0,0 +1,6 @@
+def Add(a,b):
+ print(a+b)
+def Sub(a,b):
+ print(a-b)
+def Div(a,b):
+ print(a/b)
---
+++ tests/calculation_tests.py
@@ -0,0 +1,4 @@
+import calculations as calculations
+print (cl.Add(20,10))
+print (cl.Sub(20,10))
+print (cl.Div(20,10))
